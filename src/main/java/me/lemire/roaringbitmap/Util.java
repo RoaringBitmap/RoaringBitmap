fa335f87ebbf27bfbd3fd568ad654b6237f36dfa
Daniel Lemire <lemire@gmail.com>
2013-09-09 10:45:55 -0400

    Smarter intersections.

--- src/main/java/me/lemire/roaringbitmap/Util.java
+++ src/main/java/me/lemire/roaringbitmap/Util.java
@@ -102,6 +102,112 @@ public final class Util {
         protected static int unsigned_intersect2by2(final short[] set1,
                 final int length1, final short[] set2, final int length2,
                 final short[] buffer) {
+                if (set1.length * 64 < set2.length) {
+                        return unsigned_onesidedgallopingintersect2by2(set1,
+                                length1, set2, length2, buffer);
+                } else if (set2.length * 64 < set1.length) {
+                        return unsigned_onesidedgallopingintersect2by2(set2,
+                                length2, set1, length1, buffer);
+                } else {
+                        return unsigned_localintersect2by2(set1, length1, set2,
+                                length2, buffer);
+                }
+        }
+
+        protected static int unsigned_onesidedgallopingintersect2by2(final short[] smallset,
+                final int smalllength, final short[] largeset, final int largelength,
+                final short[] buffer) {
+                if (0 == smalllength)
+                        return 0;
+                int k1 = 0;
+                int k2 = 0;
+                int pos = 0;
+                mainwhile: while (true) {
+                        if (toIntUnsigned(largeset[k1]) < toIntUnsigned(smallset[k2])) {
+                                k1 = advanceUntil(largeset, k1, largelength, smallset[k2]);
+                                if (k1 == largelength)
+                                        break mainwhile;
+                        }
+                        if (toIntUnsigned(smallset[k2]) < toIntUnsigned(largeset[k1])) {
+                                ++k2;
+                                if (k2 == smalllength)
+                                        break mainwhile;
+                        } else {
+                                // (set2[k2] == set1[k1])
+                                buffer[pos++] = smallset[k2];
+                                ++k2;
+                                if (k2 == smalllength)
+                                        break;
+                                k1 = advanceUntil(largeset, k1, largelength, smallset[k2]);
+                                if (k1 == largelength)
+                                        break mainwhile;
+                        }
+
+                }
+                return pos;
+
+        }
+
+
+        /**
+         * Find the smallest integer larger than pos such that array[pos]>= min. If
+         * none can be found, return length. Based on code by O. Kaser.
+         * 
+         * @param array
+         * @param pos
+         * @param min
+         * @return x greater than pos such that array[pos] is at least as large as min, pos is is equal to length if it is not possible.
+         */
+        private static int advanceUntil(short[] array, int pos, int length, short min) {
+                int lower = pos + 1;
+
+                // special handling for a possibly common sequential case
+                if (lower >= length || toIntUnsigned(array[lower]) >= toIntUnsigned(min)) {
+                        return lower;
+                }
+
+                int spansize = 1; // could set larger
+                // bootstrap an upper limit
+
+                while (lower + spansize < length && toIntUnsigned(array[lower + spansize]) < toIntUnsigned(min))
+                        spansize *= 2; // hoping for compiler will reduce to shift
+                int upper = (lower + spansize < length) ? lower + spansize : length - 1;
+
+                // maybe we are lucky (could be common case when the seek ahead expected
+                // to be small and sequential will otherwise make us look bad)
+                if (array[upper] == min) {
+                        return upper;
+                }
+
+                if (toIntUnsigned(array[upper]) < toIntUnsigned(min)) {// means array has no item >= min
+                        // pos = array.length;
+                        return length;
+                }
+
+                // we know that the next-smallest span was too small
+                lower += (spansize / 2);
+
+                // else begin binary search
+                // invariant: array[lower]<min && array[upper]>min
+                int mid = 0;
+                while (lower + 1 != upper) {
+                        mid = (lower + upper) / 2;
+                        if (array[mid] == min) {
+                                return mid;
+                        } else if (toIntUnsigned(array[mid]) < toIntUnsigned(min))
+                                lower = mid;
+                        else
+                                upper = mid;
+                }
+                return upper;
+
+        }
+
+
+        
+        protected static int unsigned_localintersect2by2(final short[] set1,
+                final int length1, final short[] set2, final int length2,
+                final short[] buffer) {
                 if ((0 == length1) || (0 == length2))
                         return 0;
                 int k1 = 0;
