4a2baa82c8975d188509f6e077ad7653ab3bc8a3
Daniel Lemire <lemire@gmail.com>
2014-02-06 19:13:53 -0500

    Real data testing

---
+++ run-real-experiments-dan.sh
@@ -0,0 +1,13 @@
+N_EXPER=1  #we have enough data to go up to 100
+mvn package -Dmaven.test.skip=true
+#sudo /home/owen/bin/cpuscaling.sh performance    #avoid on demand stuff
+echo turbo boost status:
+#turboboost.sh off
+#full weather lead to crash   weather_large_withjan82.csv 
+
+for ds in census-income.csv  ; do
+    java -cp ./target/RoaringBitmap-0.0.1-SNAPSHOT.jar:./target/lib/*  -javaagent:./lib/SizeOf.jar me.lemire.roaringbitmap.experiments.BenchmarkReal real-roaring-datasets $ds $N_EXPER
+done
+
+
+
--- run-real-experiments.sh
+++ run-real-experiments.sh
@@ -6,7 +6,7 @@ turboboost.sh off
 #full weather lead to crash   weather_large_withjan82.csv 
 
 for ds in census1881.csv census-income.csv wikileaks-noquotes.csv uscensus2000.csv weather_sept_85.csv ; do
-    java -cp ~/lemur-git/RoaringBitmap/target/RoaringBitmap-0.0.1-SNAPSHOT.jar:$CLASSPATH -javaagent:/home/owen/lemur-git/RoaringBitmap/lib/SizeOf.jar me.lemire.roaringbitmap.experiments.BenchmarkReal real-roaring-datasets $ds $N_EXPER
+    java -cp ~/lemur-git/RoaringBitmap/target/RoaringBitmap-0.0.1-SNAPSHOT.jar:$CLASSPATH -javaagent:./lib/SizeOf.jar me.lemire.roaringbitmap.experiments.BenchmarkReal real-roaring-datasets $ds $N_EXPER
 done
 
 
