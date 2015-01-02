650810f1aef337121f57edfb92f468f0cf903ed7
Daniel Lemire <lemire@gmail.com>
2015-01-02 12:42:44 -0500

    Simplifying the test

--- examples/run.sh
+++ examples/run.sh
@@ -1,7 +1,4 @@
-if test -n "$(find ../target/RoaringBitmap-*.jar -maxdepth 1 -name 'files*' -print -quit)"
-then
-    cd .. && mvn -Dmaven.test.skip=true package && cd examples 
-fi
+cd .. && mvn -Dmaven.test.skip=true package && cd examples 
 echo "Running SerializeToByteBufferExample"
 javac -cp "../target/*" SerializeToByteBufferExample.java && java -cp ../target/*:. SerializeToByteBufferExample
 echo
