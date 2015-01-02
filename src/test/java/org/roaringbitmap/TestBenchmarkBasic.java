780616b726082e8e875737f16c85fdb4e7a2482d
Daniel Lemire <lemire@gmail.com>
2015-01-02 10:38:25 -0500

    Updating the other benchmarks...

--- src/test/java/org/roaringbitmap/TestBenchmarkBasic.java
+++ src/test/java/org/roaringbitmap/TestBenchmarkBasic.java
@@ -1,39 +1,104 @@
 package org.roaringbitmap;
 
+import static org.roaringbitmap.buffer.BenchmarkConsumers.CONSOLE_CONSUMER;
+import static org.roaringbitmap.buffer.BenchmarkConsumers.H2_CONSUMER;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import com.carrotsearch.junitbenchmarks.BenchmarkRule;
 
 public class TestBenchmarkBasic {
     
+
+    @Rule
+    public TestRule benchmarkRun = new BenchmarkRule(CONSOLE_CONSUMER, H2_CONSUMER);
+
+    @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
+    @Test
+    public void bigunion() throws Exception {
+        for (int k = 1; k < N; k += 10) {
+            RoaringBitmap bitmapor = FastAggregation.horizontal_or(Arrays.copyOf(ewah, k + 1));
+            bogus += bitmapor.getCardinality();
+        }
+    }
+
+
+
+    @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
+    @Test
+    public void toarray() throws Exception {
+        for (int k = 1; k < N * 100; ++k) {
+            bogus += ewah[k % N].toArray().length;
+        }
+    }
+
+
+    @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
+    @Test
+    public void cardinality() throws Exception {
+        for (int k = 1; k < N * 100; ++k) {
+            bogus += ewah[k % N].getCardinality();
+        }
+    }
+
+
+    @BeforeClass
+    public static void prepare() throws IOException {
+        for (int k = 0; k < N; ++k) {
+            ewah[k] = new RoaringBitmap();
+            for (int x = 0; x < M; ++x) {
+                ewah[k].add(x * (N - k + 2));
+            }
+            ewah[k].trim();
+        }
+    }
+
+    @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
     @Test
     public void createBitmapOrdered() {
         long besttime = Long.MAX_VALUE;
-        for (int j = 0; j < 100; ++j) {
-            RoaringBitmap r = new RoaringBitmap();
-            long bef = System.nanoTime();
-            for (int k = 0; k < 65536; k++) {
-                r.add(k * 32);
-            }
-            long aft = System.nanoTime();
-            if(besttime > aft - bef) besttime = aft-bef;
+        RoaringBitmap r = new RoaringBitmap();
+        long bef = System.nanoTime();
+        for (int k = 0; k < 65536; k++) {
+            r.add(k * 32);
         }
-        System.out.println("Ordered creation time " + besttime);
+        long aft = System.nanoTime();
+        if(besttime > aft - bef) besttime = aft-bef;
     }
     
+    @BenchmarkOptions(benchmarkRounds = 10, warmupRounds = 3)
     @Test
     public void createBitmapUnordered() {
         long besttime = Long.MAX_VALUE;
-        for (int j = 0; j < 100; ++j) {
-            RoaringBitmap r = new RoaringBitmap();
-            long bef = System.nanoTime();
-            for (int k = 65536-1; k >=0 ; k--) {
-                r.add(k * 32);
-            }
-            long aft = System.nanoTime();
-            if(besttime > aft - bef) besttime = aft-bef;
+        RoaringBitmap r = new RoaringBitmap();
+        long bef = System.nanoTime();
+        for (int k = 65536 - 1; k >= 0; k--) {
+            r.add(k * 32);
         }
-        System.out.println("Reverse creation time " + besttime);
-
+        long aft = System.nanoTime();
+        if (besttime > aft - bef)
+            besttime = aft - bef;
     }
 
+
+    static int N = 1000;
+    static int M = 1000;
+
+    static RoaringBitmap[] ewah = new RoaringBitmap[N];
+
+    public static int bogus = 0;
 
 }
