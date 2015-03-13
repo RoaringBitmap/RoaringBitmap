711c4ff6824ae4fe408a163002fd0049e5337dd1
Daniel Lemire <lemire@gmail.com>
2015-03-13 12:04:28 -0400

    Taking Flyweight for a spin.

--- src/main/java/org/roaringbitmap/IntIteratorFlyweight.java
+++ src/main/java/org/roaringbitmap/IntIteratorFlyweight.java
@@ -1,15 +1,11 @@
 package org.roaringbitmap;
 
 /**
- * This Iterator will let you go green and make your garbage collector suffer less.
+ * Fast iterator minimizing the stress on the garbage collector.
  *
  * You can create one reusable instance of this class and then {@link #wrap(RoaringBitmap)}
- *
- * I am aware of the code duplication and the VeryLongNamingOfTheIterators but in order
- * to fix this it will take to make a lot of changes to the other Iterators in this project as well.
- *
- *
- */
+ * 
+ * */
 
 
 public class IntIteratorFlyweight implements IntIterator {
