2a20615bad57acbc892bdb47601df12d77b65178
Daniel Lemire <lemire@gmail.com>
2015-04-01 10:09:00 -0400

    Adding new benchmark.

---
+++ jmh/src/main/java/org/roaringbitmap/aggregation/newand/identical/RoaringBitmapBenchmark.java
@@ -0,0 +1,53 @@
+package org.roaringbitmap.aggregation.newand.identical;
+
+import org.openjdk.jmh.annotations.*;
+import org.roaringbitmap.RoaringBitmap;
+
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+public class RoaringBitmapBenchmark {
+
+    private RoaringBitmap bitmap1;
+    private RoaringBitmap bitmap2;
+
+    @Setup
+    public void setup() {
+        bitmap1 = new RoaringBitmap();
+        bitmap2 = new RoaringBitmap();
+        int k = 1 << 16;
+        for(int i = 0; i < 10000; ++i) {
+            bitmap1.add(i * k);
+            bitmap2.add(i * k);
+        }
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void and() {
+        RoaringBitmap.and(bitmap1, bitmap2);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void inplace_and() {
+        bitmap1.and(bitmap2);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void newand() {
+        RoaringBitmap.newand(bitmap1, bitmap2);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void inplace_newand() {
+        bitmap1.newand(bitmap2);
+    }
+
+}
