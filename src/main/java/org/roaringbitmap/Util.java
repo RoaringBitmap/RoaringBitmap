887417159bece7546eeba299a4695ab2e8c43a7a
Daniel Lemire <lemire@gmail.com>
2015-04-10 14:48:47 -0400

    Improving select performance (Bob Potter)

--- src/main/java/org/roaringbitmap/Util.java
+++ src/main/java/org/roaringbitmap/Util.java
@@ -480,49 +480,49 @@ public final class Util {
      * @return position of jth true bit in w
      */
     public static int select(long w, int j) {
-        int part1 = (int)( w & 0xFFFFFFFF);
-        int wfirsthalf = Integer.bitCount(part1);
-        if(wfirsthalf > j ) {
-            return select(part1,j); 
-        } else {
-            return select((int)(w>>>32),j - wfirsthalf) + 32;
+        int seen = 0;
+        // Divide 64bit
+        int part = (int) (w & 0xFFFFFFFF);
+        int n = Integer.bitCount(part);
+        if (n <= j) {
+            part = (int) (w >>> 32);
+            seen += 32;
+            j -= n;
         }
-    }
+        int ww = part;
 
-    /**
-     * Given a word w, return the position of the jth true bit.
-     * 
-     * @param w word
-     * @param j index
-     * @return position of jth true bit in w
-     */
-    public static int select(int w, int j) {
-        int part1 =  w & 0xFFFF;
-        int wfirsthalf = Integer.bitCount(part1);
-        if(wfirsthalf > j ) {
-            return select((short)part1,j); 
-        } else {
-            return select((short)(w>>>16),j-wfirsthalf) + 16;
-        }
-    }
-    
+        // Divide 32bit
+        part = ww & 0xFFFF;
 
-    /**
-     * Given a word w, return the position of the jth true bit.
-     * 
-     * @param w word
-     * @param j index
-     * @return position of jth true bit in w
-     */
-    public static int select(short w, int j) {
-        int sumtotal = 0;
-        for(int counter = 0;  counter < 16 ; ++counter) {
-            sumtotal += (w >> counter) & 1;
-            if(sumtotal > j) return counter;
+        n = Integer.bitCount(part);
+        if (n <= j) {
+
+            part = ww >>> 16;
+            seen += 16;
+            j -= n;
         }
-        throw new IllegalArgumentException("cannot local "+j+"th bit in "+w+" weight is "+Integer.bitCount(w));
+        ww = part;
+
+        // Divide 16bit
+        part = ww & 0xFF;
+        n = Integer.bitCount(part);
+        if (n <= j) {
+            part = ww >>> 8;
+            seen += 8;
+            j -= n;
+        }
+        ww = part;
+
+        // Lookup in final byte
+        int counter;
+        for (counter = 0; counter < 8; counter++) {
+            j -= (ww >>> counter) & 1;
+            if (j < 0) {
+                break;
+            }
+        }
+        return seen + counter;
     }
-    
     
 
     /**
