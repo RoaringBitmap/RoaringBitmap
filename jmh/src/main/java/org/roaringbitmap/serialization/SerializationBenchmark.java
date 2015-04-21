1551e3b3d8cb0ccf6ecbc8c9cf0060996b116161
Daniel Lemire <lemire@gmail.com>
2015-04-21 16:56:33 -0400

    Fixing serialization wrapper.

--- jmh/src/main/java/org/roaringbitmap/serialization/SerializationBenchmark.java
+++ jmh/src/main/java/org/roaringbitmap/serialization/SerializationBenchmark.java
@@ -119,36 +119,62 @@ public class SerializationBenchmark {
 }
 
 
-
 class ByteBufferBackedInputStream extends InputStream {
-  
-  ByteBuffer buf;
-  ByteBufferBackedInputStream( ByteBuffer buf){
-    this.buf = buf;
-  }
-  public synchronized int read() throws IOException {
-    if (!buf.hasRemaining()) {
-      return -1;
+    
+    ByteBuffer buf;
+    ByteBufferBackedInputStream( ByteBuffer buf){
+      this.buf = buf;
     }
-    return buf.get();
-  }
-  public synchronized int read(byte[] bytes, int off, int len) throws IOException {
-    len = Math.min(len, buf.remaining());
-    buf.get(bytes, off, len);
-    return len;
-  }
-}
-class ByteBufferBackedOutputStream extends OutputStream{
-  ByteBuffer buf;
-  ByteBufferBackedOutputStream( ByteBuffer buf){
-    this.buf = buf;
-  }
-  public synchronized void write(int b) throws IOException {
-    buf.put((byte) b);
+    public int read() throws IOException {
+      if (!buf.hasRemaining()) {
+        return -1;
+      }
+      return 0xFF & buf.get();
+    }
+    public int read(byte[] bytes) throws IOException {
+        int len = Math.min(bytes.length, buf.remaining());
+        buf.get(bytes, 0, len);
+        return len;
+     }
+    
+    public long skip(long n) {
+        int len = Math.min((int)n, buf.remaining());
+        buf.position(buf.position() + (int)n);
+        return len;
+    }
+    
+    public int available() throws IOException {
+        return buf.remaining();
+    }
+    
+    public boolean markSupported() {
+        return false;
+    }
+        
+    public int read(byte[] bytes, int off, int len) throws IOException {
+      len = Math.min(len, buf.remaining());
+      buf.get(bytes, off, len);
+      return len;
+    }
   }
 
-  public synchronized void write(byte[] bytes, int off, int len) throws IOException {
-    buf.put(bytes, off, len);
+  class ByteBufferBackedOutputStream extends OutputStream{
+    ByteBuffer buf;
+    ByteBufferBackedOutputStream( ByteBuffer buf){
+      this.buf = buf;
+    }
+    public synchronized void write(int b) throws IOException {
+      buf.put((byte) b);
+    }
+
+    public synchronized void write(byte[] bytes) throws IOException {
+      buf.put(bytes);
+    }
+    
+    public synchronized void write(byte[] bytes, int off, int len) throws IOException {
+      buf.put(bytes, off, len);
+    }
+    
   }
-  
-}
+
+
