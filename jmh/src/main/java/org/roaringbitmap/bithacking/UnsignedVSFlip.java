1219f075895d53ba176520e6d31ee8eef336f7d5
Daniel Lemire <lemire@gmail.com>
2015-06-12 19:05:54 -0400

    Updated according to Greg's.

--- jmh/src/main/java/org/roaringbitmap/bithacking/UnsignedVSFlip.java
+++ jmh/src/main/java/org/roaringbitmap/bithacking/UnsignedVSFlip.java
@@ -6,46 +6,28 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class UnsignedVSFlip {
-    short[] key;
-    int N;
+    @Param({"1", "31", "65", "101", "103"})
+    public short key;
     
-    @Setup
-    public void setup() {
-        Random r = new Random();
-        N = 65536;
-        key = new short[N];
-        for(int k = 0; k < N; ++k)
-            while(key[k] == 0) 
-            	key[k] = (short)r.nextInt();        
-    }
     
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public int tointUnsignedTime() {
-        int answer = 0;
-        for(int k = 0; k < N; ++k)
-            answer += key[k] & 0xFFFF;
-        return answer;
+        return key & 0xFFFF;
     }
     
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public int flipTime() {
-        int answer = 0;
-        for(int k = 0; k < N; ++k)
-            answer += (short) (key[k] ^ Short.MIN_VALUE);
-        return answer;
+        return key ^ Short.MIN_VALUE;
     }
-    
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    public int flipTimeNoCast() {
-        int answer = 0;
-        for(int k = 0; k < N; ++k)
-            answer += key[k] ^ Short.MIN_VALUE;
-        return answer;
+    public short flipTimeShort() {
+        return (short) (key ^ Short.MIN_VALUE);
     }
+    
 }
