9296937d3c79f2ad133bf2fe7a4e6fc65390ae99
Daniel Lemire <lemire@gmail.com>
2015-11-13 10:13:41 -0500

    Adding a new example...

--- examples/run.sh
+++ examples/run.sh
@@ -1,4 +1,7 @@
-cd .. && mvn -Dmaven.test.skip=true package && cd examples 
+cd .. && mvn -Dmaven.test.skip=true package && cd examples
+echo "Running CompressionResults"
+javac -cp "../target/*" CompressionResults.java && java -cp ../target/*:. CompressionResults
+echo
 echo "Running SerializeToByteBufferExample"
 javac -cp "../target/*" SerializeToByteBufferExample.java && java -cp ../target/*:. SerializeToByteBufferExample
 echo
@@ -8,7 +11,7 @@ echo
 echo "Running MemoryMappingExample"
 javac -cp "../target/*" MemoryMappingExample.java && java -cp ../target/*:. MemoryMappingExample
 echo
-echo "Serializing to byte array" 
+echo "Serializing to byte array"
 javac -cp "../target/*":. SerializeToByteArrayExample.java && java -cp ../target/*:. SerializeToByteArrayExample
 
 rm *.class
