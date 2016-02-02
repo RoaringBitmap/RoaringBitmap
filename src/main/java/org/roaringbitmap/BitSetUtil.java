8f69ece1a6d8628e4b56f5c6886995a7cb997397
Olaf Krische <public@ecopatz.de>
2016-02-02 21:49:27 +0100

    implement transformation to RoaringBitmap via long[] as well

--- src/main/java/org/roaringbitmap/BitSetUtil.java
+++ src/main/java/org/roaringbitmap/BitSetUtil.java
@@ -1,190 +1,124 @@
 package org.roaringbitmap;
 
+import java.util.Arrays;
 import java.util.BitSet;
 
 public class BitSetUtil {
-	static final int CHUNK_SIZE = 1 << 16;
+	// a block consists has a maximum of 1024 words, each representing 64 bits, thus representing at maximum 65536 bits
+	static final int BLOCK_LENGTH = 1024; 
 
 	/**
-	 * Generate RoaringBitmap out of the given bitSet
+	 * Generate RoaringBitmap out of the bitSet
 	 * 
-	 * @param bitset
-	 * @return bitmap
+	 * @param bitSet
+	 * @return roaring bitmap
 	 */
-	static RoaringBitmap bitmapOf(final BitSet bitset) {
-		// dont make method public, so other parties wont use it.
-		// if daniel likes it, he will put it to RoaringBitmap :>
-		final int bitsetLength = bitset.length();
-		if (bitsetLength == 0) {
+	static RoaringBitmap bitmapOf(final BitSet bitSet) {
+		final int cardinality = bitSet.cardinality();
+		if (cardinality == 0) {
 			return new RoaringBitmap();
+		} else if (cardinality <= ArrayContainer.DEFAULT_MAX_SIZE) {
+			// if it is a tiny bitSet, we can directly build a arrayContainer, without an extra bitSet#toLongArray
+			final RoaringBitmap bitmap = new RoaringBitmap();
+			bitmap.highLowContainer.append((short)0, arrayContainerOf(cardinality, bitSet));
+			return bitmap;
+		} else {
+			return bitmapOf(cardinality, bitSet.toLongArray());
 		}
+	}
 
+	/**
+	 * Generate RoaringBitmap out of a long[], each long using little-endian representation of its bits 
+	 * 
+	 * @see BitSet#toLongArray() for an equivalent
+	 * @param bitset
+	 * @return roaring bitmap
+	 */
+    static RoaringBitmap bitmapOf(final long[] words) {
+		final int overallCardinality = cardinality(0, words.length, words);
+		if (overallCardinality == 0) {
+			return new RoaringBitmap();
+		} else {
+			return bitmapOf(overallCardinality, words);
+		}
+    }
+    
+	private static RoaringBitmap bitmapOf(final int cardinality, final long[] words) {
+		// split long[] into blocks. 
+		// each block becomes a single container, if any bit is set
 		final RoaringBitmap ans = new RoaringBitmap();
-
-		// divide bitset in 1<<16 chunks
-		final int chunkSize = 1 << 16;
-		for (int offset = 0; offset < bitsetLength; offset += chunkSize) {
-			// avoid empty container creation, if there is no bit in range anyways
-			if (BitSetUtil.any(offset, chunkSize, bitset)) {
-				final Container container = BitSetUtil.containerOf(offset, chunkSize, bitset);
-
-				// it is always the first container, it never replaces or expands an existing one
-				// => index is always negative, no extra check needed
-				final short hb = Util.highbits(offset);
-				final int i = ans.highLowContainer.getIndex(hb);
-				ans.highLowContainer.insertNewKeyValueAt(-i - 1, hb, container);
+		int containerIndex = 0;
+		int currentCardinality = 0;
+		
+		// for each block
+		// (unless we already have reached overall cardinality, e.g. when long[] is way longer and has no bits set, then we can skip everything else)
+		for (int from = 0; from < words.length && currentCardinality < cardinality; from += BLOCK_LENGTH) {
+			final int to = Math.min(from + BLOCK_LENGTH, words.length);
+			final int blockCardinality = cardinality(from, to, words);
+			if (blockCardinality > 0) { 
+				ans.highLowContainer.insertNewKeyValueAt(containerIndex++, Util.highbits(from * Long.SIZE), BitSetUtil.containerOf(from, to, blockCardinality, words));
+				currentCardinality += blockCardinality;
 			}
 		}
 		return ans;
 	}
 
-	private static Container containerOf(final int offset, final int length, final BitSet bitSet) {
-		// TODO: find a good way to decide between arraycontainer/bitmapcontainer and runcontainer, without using lots of allocations
-		final int cardinality = cardinality(offset, length, bitSet);
-		if (cardinality <= ArrayContainer.DEFAULT_MAX_SIZE) {
-			// containers with DEFAULT_MAX_SZE or less integers should be ArrayContainers
-			return arrayContainerOf(offset, length, cardinality, bitSet);
+	private static Container containerOf(final int from, final int to, final int blockCardinality, final long[] words) {
+		// find the best container available
+		if (blockCardinality <= ArrayContainer.DEFAULT_MAX_SIZE) {
+			// containers with DEFAULT_MAX_SIZE or less integers should be ArrayContainers
+			return arrayContainerOf(from, to, blockCardinality, words);
+		} else {
+			// otherwise use bitmap container, which is kinda the same as Bitset
+			// ideas for the future: 
+			// - if blockCardinality is rather high, then runlength encoding via RunContainer might be the better choice) 
+			// - (maybe there is also another way of recognizing high amount of repetition? i would rather scan the words once in the beginning and save memory at the end?)	
+			return new BitmapContainer(Arrays.copyOfRange(words, from, to), blockCardinality);
 		}
-
-		final int runs = BitSetUtil.nbrRuns(offset, length, bitSet);
-		if (runs == 0) {
-			return new RunContainer();
-		}
-		return runContainerOf(offset, length, runs, bitSet);
 	}
 
-	private static ArrayContainer arrayContainerOf(final int offset, final int length, final int cardinality, final BitSet bs) {
-		// content is sorted, as BitSet is already sorted  
+	private static ArrayContainer arrayContainerOf(final int from, final int to, final int cardinality, final long[] words) {
+		// precondition: cardinality is max 4096
 		final short[] content = new short[cardinality];
-		int j = 0;
-		for (int i = bs.nextSetBit(offset), offsetLength = offset + length; i >= 0 && i < offsetLength; i = bs.nextSetBit(i + 1)) {
-			content[j++] = (short) (i - offset);
+		int index = 0;
+						
+		// for each word, unless we already have reached cardinality
+		long word = 0;
+		for (int i = from, socket = 0; i < to && index < cardinality; i++, socket += Long.SIZE) { 
+			if (words[i] == 0) continue;
+			
+			// for each bit, unless updated word has become 0 (no more bits left) or we already have reached cardinality
+			word = words[i];
+			for (int bitIndex = 0; word != 0 && bitIndex < Long.SIZE && index < cardinality; word >>>= 1, bitIndex++) {
+				// TODO: does it make sense to start with (0+trailing zeros) or not worth the effort? 
+				// (what could be the fastest way to iterate through the long?)
+				if ((word & 1l) != 0) {
+					content[index++] = (short)(socket + bitIndex);
+				}
+			}
 		}
 		return new ArrayContainer(content);
 	}
 
-	private static RunContainer runContainerOf(final int offset, final int length, final int nbrRuns, final BitSet bs) {
-		if (nbrRuns == 0) {
-			return new RunContainer(new short[0], 0);
+	private static ArrayContainer arrayContainerOf(final int cardinality, final BitSet bs) {
+		final short[] content = new short[cardinality];
+		int index = 0;
+	    for (int i = bs.nextSetBit(0); i >= 0; i = bs.nextSetBit(i+1)) {
+	             // operate on index i here
+	             if (i == Integer.MAX_VALUE) {
+	                 break; // or (i+1) would overflow
+	             }
+	             content[index++] = (short)i;
+	    }
+	    return new ArrayContainer(content);
+	};
+	
+
+	private static int cardinality(final int from, final int to, final long[] words) {
+		int sum = 0;
+		for (int i = from; i < to; i++) {
+			sum += Long.bitCount(words[i]);
 		}
-
-		short[] valueslength = new short[2 * nbrRuns];
-
-		int index = -1;
-		int runLength = 0;
-		boolean finish = false;
-		int runs = 0;
-		for (int i = bs.nextSetBit(offset), offsetLength = offset + length; i >= 0 && i < offsetLength; i = bs.nextSetBit(i + 1)) {
-			// operate on index i here
-			if (i == Integer.MAX_VALUE) {
-				if (index != -1) {
-					valueslength[runs * 2] = (short) (index - offset);
-					valueslength[runs * 2 + 1] = (short) runLength;
-					runs++;
-				}
-				finish = true;
-				break; // or (i+1) would overflow
-			}
-
-			if (index == -1) {
-				index = i;
-				runLength = 0;
-			} else {
-				if (index + runLength + 1 == i) {
-					runLength++;
-				} else {
-					valueslength[runs * 2] = (short) (index - offset);
-					valueslength[runs * 2 + 1] = (short) runLength;
-					runs++;
-
-					index = i;
-					runLength = 0;
-				}
-			}
-		}
-
-		if (index != -1 && !finish) {
-			valueslength[runs * 2] = (short) (index - offset);
-			valueslength[runs * 2 + 1] = (short) runLength;
-			runs++;
-		}
-
-		return new RunContainer(valueslength, nbrRuns);
-	}
-
-	/**
-	 * How many runs of consecutive values are necessary to represents the set
-	 * bits in the range of the bitSet
-	 * 
-	 * @param offset
-	 * @param length
-	 * @param bitSet
-	 * @return nbrruns
-	 */
-	private static int nbrRuns(final int offset, final int length, final BitSet bitSet) {
-		int index = -1;
-		int runLength = 0;
-		boolean finish = false;
-		int runs = 0;
-		for (int i = bitSet.nextSetBit(offset), offsetLength = offset + length; i >= 0 && i < offsetLength; i = bitSet.nextSetBit(i + 1)) {
-			// operate on index i here
-			if (i == Integer.MAX_VALUE) {
-				if (index != -1) {
-					runs++;
-				}
-				finish = true;
-				break; // or (i+1) would overflow
-			}
-
-			if (index == -1) {
-				index = i;
-				runLength = 0;
-			} else {
-				if (index + runLength + 1 == i) {
-					runLength++;
-				} else {
-					runs++;
-					index = i;
-					runLength = 0;
-				}
-			}
-		}
-
-		if (index != -1 && !finish) {
-			runs++;
-		}
-		return runs;
-	}
-
-	/**
-	 * Returns true, if any bit is set in range of the bitSet
-	 * 
-	 * @param offset
-	 * @param length
-	 * @param bitSet
-	 * @return true, if any bit is set in range of the bitSet
-	 */
-	private static boolean any(final int offset, final int length, final BitSet bitSet) {
-		final int index = bitSet.nextSetBit(offset);
-		return index >= 0 && index < offset + length;
-	}
-
-	/**
-	 * Counts set bits in the range of the bitSet
-	 * 
-	 * @param offset
-	 * @param length
-	 * @param bitSet
-	 * @return counted set bits in the range of the bitSet
-	 */
-	private static int cardinality(final int offset, final int length, final BitSet bitSet) {
-		int cardinality = 0;
-		for (int i = bitSet.nextSetBit(offset), offsetLength = offset + length; i >= 0 && i < offsetLength; i = bitSet.nextSetBit(i + 1)) {
-			if (i == Integer.MAX_VALUE) {
-				break; // or (i+1) would overflow
-			}
-			cardinality++;
-		}
-		return cardinality;
+		return sum;
 	}
 }
--- src/test/java/org/roaringbitmap/TestBitSetUtil.java
+++ src/test/java/org/roaringbitmap/TestBitSetUtil.java
@@ -18,7 +18,36 @@ public class TestBitSetUtil {
 	@Test
 	public void testFullBitSet() {
 		final BitSet bitset = new BitSet();
-		bitset.set(0, BitSetUtil.CHUNK_SIZE * 10);
+		final int nbits = BitSetUtil.BLOCK_LENGTH * Long.SIZE * 50;
+		bitset.set(0, nbits);
+		final RoaringBitmap bitmap = BitSetUtil.bitmapOf(bitset);
+		assertEqualBitsets(bitset, bitmap);
+	}
+	
+	@Test
+	public void testFlipFlapBetweenRandomFullAndEmptyBitSet() {		
+		final Random random = new Random();	
+		final int nbitsPerBlock = BitSetUtil.BLOCK_LENGTH * Long.SIZE;
+		final int blocks = 50;
+		final BitSet bitset = new BitSet(nbitsPerBlock*blocks);
+		
+		// i want a mix of empty blocks, randomly filled blocks and full blocks
+		for (int block = 0; block < blocks * nbitsPerBlock; block+=nbitsPerBlock) {
+			int type = random.nextInt(3);
+			switch(type) {
+				case 0:
+					// a block with random set bits
+					appendRandomBitset(random, block, bitset, nbitsPerBlock);
+					break;
+				case 1:
+					// a full block
+					bitset.set(block, block+nbitsPerBlock);
+					break;
+				default:
+					// and an empty block; 
+					break;
+			}
+		}
 		final RoaringBitmap bitmap = BitSetUtil.bitmapOf(bitset);
 		assertEqualBitsets(bitset, bitmap);
 	}
@@ -26,8 +55,10 @@ public class TestBitSetUtil {
 	@Test
 	public void testRandomBitmap() {
 		final Random random = new Random();
-		for (int i = 0; i < 10; i++) {
-			final BitSet bitset = randomBitset(random, random.nextInt(BitSetUtil.CHUNK_SIZE * 10));
+		final int runs = 500;
+		final int maxNbits = 500000;
+		for (int i = 0; i < runs; i++) {
+			final BitSet bitset = randomBitset(random, random.nextInt(maxNbits));
 			final RoaringBitmap bitmap = BitSetUtil.bitmapOf(bitset);
 			assertEqualBitsets(bitset, bitmap);
 		}
@@ -41,11 +72,15 @@ public class TestBitSetUtil {
 		}
 	}
 
-	private static BitSet randomBitset(final Random random, final int length) {
-		final BitSet bitset = new BitSet();
-		for (int i = 0; i < length; i++) {
-			bitset.set(i, random.nextBoolean());
+	private static BitSet appendRandomBitset(final Random random, final int offset, final BitSet bitset, final int nbits) {
+		for (int i = 0; i < nbits; i++) {
+			bitset.set(offset+i, random.nextBoolean());
 		}
 		return bitset;
 	}
+	private static BitSet randomBitset(final Random random, final int length) {
+		final BitSet bitset = new BitSet();
+		return appendRandomBitset(random, 0, bitset, length); 
+	}
+
 }
