f0ae564b4ae325db5b2348711b7c8efbfcb1774a
Daniel Lemire <lemire@gmail.com>
2016-03-17 20:41:20 -0400

    Adding reproducible benchmarks for spe150271 paper.

---
+++ jmh/src/main/java/org/roaringbitmap/spe150271/runroaring/RealDataBenchmarkWideOrNaive.java
@@ -0,0 +1,22 @@
+package org.roaringbitmap.spe150271.runroaring;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.roaringbitmap.spe150271.runroaring.state.RealDataBenchmarkState;
+import org.roaringbitmap.realdata.wrapper.BitmapAggregator;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class RealDataBenchmarkWideOrNaive {
+
+   @Benchmark
+   public int wideOr_naive(RealDataBenchmarkState bs) {
+      BitmapAggregator aggregator = bs.bitmaps.get(0).naiveOrAggregator();
+      return aggregator.aggregate(bs.bitmaps).cardinality();
+   }
+
+}
