c0c68bb31ac05ab279e7d1576f9c5cfb790047d4
Daniel Lemire <lemire@gmail.com>
2016-03-17 21:04:06 -0400

    Adding bash headers

--- jmh/grabresults.sh
+++ jmh/grabresults.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 echo "Parsing results from log file " $1
 grep -A 10000 "# Run complete." $1 | awk '{print $2 " "  $1 " " $5}' | sort  |rev | column -t |rev
--- jmh/run.sh
+++ jmh/run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 BASEDIR=$(dirname $0)
 
 
