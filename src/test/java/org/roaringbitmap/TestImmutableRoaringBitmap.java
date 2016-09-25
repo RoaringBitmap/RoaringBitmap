6fc2c7816a2e10e5c08b805af00ff9eb6290fd9e
gssiyankai <syk.greg@gmail.com>
2016-09-25 10:12:57 +0200

    Improve code coverage

--- src/test/java/org/roaringbitmap/TestImmutableRoaringBitmap.java
+++ src/test/java/org/roaringbitmap/TestImmutableRoaringBitmap.java
@@ -25,4 +25,33 @@ public class TestImmutableRoaringBitmap {
         assertEquals(expected, ImmutableRoaringBitmap.or(b, a));
     }
 
+    @Test
+    public void andNot() {
+        ImmutableRoaringBitmap a = ImmutableRoaringBitmap.bitmapOf(1<<16, 2<<16);
+        ImmutableRoaringBitmap b = ImmutableRoaringBitmap.bitmapOf(11, 12, 13, 2<<16);
+        ImmutableRoaringBitmap andNot = ImmutableRoaringBitmap.andNot(a, b);
+        ImmutableRoaringBitmap expected = ImmutableRoaringBitmap.bitmapOf(1<<16);
+        assertEquals(expected, andNot);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void flipInvalidRange() {
+        ImmutableRoaringBitmap a = ImmutableRoaringBitmap.bitmapOf(1, 5, 7, 13);
+        ImmutableRoaringBitmap.flip(a, 7L, 5L);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void flipInvalidRange2() {
+        ImmutableRoaringBitmap a = ImmutableRoaringBitmap.bitmapOf(1, 5, 7, 13);
+        ImmutableRoaringBitmap.flip(a, 1L<<32, 1L<<33);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void flipInvalidRange3() {
+        ImmutableRoaringBitmap a = ImmutableRoaringBitmap.bitmapOf(1, 5, 7, 13);
+        ImmutableRoaringBitmap.flip(a, 1L, 1L<<33);
+    }
+
+
+
 }
