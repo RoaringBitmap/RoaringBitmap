13170cd6fbadcab8c16de1986ad80b2564b7d238
Daniel Lemire <lemire@gmail.com>
2017-01-23 17:55:52 -0500

    Justifying IntIterator.

--- src/main/java/org/roaringbitmap/IntIterator.java
+++ src/main/java/org/roaringbitmap/IntIterator.java
@@ -5,7 +5,10 @@
 package org.roaringbitmap;
 
 /**
- * A simple iterator over integer values
+ * A simple iterator over integer values.
+ * Using an IntIterator instead of Java's Iterator&lt;Integer&gt;
+ * avoids the overhead of the Interger class: on some tests,
+ * IntIterator is nearly twice as fast as Iterator&lt;Integer&gt;.
  */
 public interface IntIterator extends Cloneable {
   /**
