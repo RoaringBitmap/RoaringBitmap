0ba86ff19ae67c6c9f781547189df96b8a7e12fc
Richard Startin <richard@openkappa.co.uk>
2018-02-01 20:52:15 +0000

    fix unsigned error in runcontainer contains runcontainer (#209)

--- roaringbitmap/src/main/java/org/roaringbitmap/RunContainer.java
+++ roaringbitmap/src/main/java/org/roaringbitmap/RunContainer.java
@@ -744,26 +744,25 @@ public final class RunContainer extends Container implements Cloneable {
   @Override
   protected boolean contains(RunContainer runContainer) {
     int i1 = 0, i2 = 0;
-    final int runCount = numberOfRuns();
-    while(i1 < runCount && i2 < runContainer.numberOfRuns()) {
-      int start1 = getValue(i1);
+    while(i1 < numberOfRuns() && i2 < runContainer.numberOfRuns()) {
+      int start1 = toIntUnsigned(getValue(i1));
       int stop1 = start1 + toIntUnsigned(getLength(i1));
-      int start2 = runContainer.getValue(i2);
+      int start2 = toIntUnsigned(runContainer.getValue(i2));
       int stop2 = start2 + toIntUnsigned(runContainer.getLength(i2));
       if(start1 > start2) {
         return false;
       } else {
         if(stop1 > stop2) {
-          i1++;
+          i2++;
         } else if(stop1 == stop2) {
           i1++;
           i2++;
         } else {
-          i2++;
+          i1++;
         }
       }
     }
-    return i1 == runCount;
+    return i2 == runContainer.numberOfRuns();
   }
 
   @Override
