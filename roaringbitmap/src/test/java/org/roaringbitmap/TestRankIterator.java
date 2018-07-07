ad986f265226f0a48e42bc8237e5dd22581c3847
Nikolay Khitrin <khitrin@gmail.com>
2018-07-07 18:00:47 +0300

    Tests for PeekableIntRankIterator.advanceIfNeeded

--- roaringbitmap/src/test/java/org/roaringbitmap/TestRankIterator.java
+++ roaringbitmap/src/test/java/org/roaringbitmap/TestRankIterator.java
@@ -1,23 +1,23 @@
 package org.roaringbitmap;
 
+import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@RunWith(Parameterized.class)
 public class TestRankIterator {
-  private void testBitmapRanksOnNext(FastRankRoaringBitmap bm) {
-    PeekableIntRankIterator iterator = bm.getIntRankIterator();
-    while (iterator.hasNext()) {
-      int bit = iterator.peekNext();
-      int rank = iterator.peekNextRank();
-      int slowRank = bm.rank(bit);
-      Assert.assertEquals(slowRank, rank);
 
-      iterator.next();
-    }
-  }
-
-  @Test
-  public void randomizedTest() {
+  @Parameterized.Parameters(name = "{index}: advance by {1}")
+  public static Collection<Object[]> parameters() {
     RoaringBitmap bm = RandomisedTestData.randomBitmap(1 << 12);
     FastRankRoaringBitmap fast = new FastRankRoaringBitmap();
     fast.or(bm);
@@ -25,6 +25,57 @@ public class TestRankIterator {
 
     Assert.assertTrue(fast.isCacheDismissed());
 
-    testBitmapRanksOnNext(fast);
+    List<Integer> primes = Arrays.asList(0, 1, 3, 5, 7, 11);
+    List<Integer> powers = IntStream.range(0, 18).mapToObj(i -> 1 << i).collect(Collectors.toList());
+
+    List<Integer> advances = Lists.cartesianProduct(primes, powers).stream()
+                                  .map(l -> l.get(0) * l.get(1))
+                                  .distinct().sorted()
+                                  .collect(Collectors.toList());
+
+    return Lists.cartesianProduct(Collections.singletonList(fast), advances)
+                .stream().map(List::toArray).collect(Collectors.toList());
+  }
+
+  @Parameterized.Parameter
+  public FastRankRoaringBitmap bitmap;
+  @Parameterized.Parameter(1)
+  public Integer advance;
+
+  @Test
+  public void testAdvance() {
+    if (advance == 0) {
+      testBitmapRanksOnNext(bitmap);
+    } else {
+      testBitmapRanksOnAdvance(bitmap, advance);
+    }
+  }
+
+  private void testBitmapRanksOnNext(FastRankRoaringBitmap bm) {
+    PeekableIntRankIterator iterator = bm.getIntRankIterator();
+    long iterations = 0;
+    while (iterator.hasNext()) {
+      ++iterations;
+      int rank = iterator.peekNextRank();
+      Assert.assertEquals(iterations, Util.toUnsignedLong(rank));
+
+      iterator.next();
+    }
+  }
+
+  private void testBitmapRanksOnAdvance(FastRankRoaringBitmap bm, int advance) {
+    PeekableIntRankIterator iterator = bm.getIntRankIterator();
+    while (iterator.hasNext()) {
+      int bit = iterator.peekNext();
+      int rank = iterator.peekNextRank();
+
+      Assert.assertEquals(bm.rank(bit), rank);
+
+      if ((Util.toUnsignedLong(bit) + advance) < 0xffffffffL) {
+        iterator.advanceIfNeeded(bit + advance);
+      } else {
+        break;
+      }
+    }
   }
 }
