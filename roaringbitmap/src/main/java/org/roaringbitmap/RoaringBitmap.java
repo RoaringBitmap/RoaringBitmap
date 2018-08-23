9234cab1cc056574affe82290c20a78498859379
Rafael Telles <raftel.ti@gmail.com>
2018-08-23 11:34:43 -0300

    Add overload of RoaringBitmap.add, to accept offset and length arguments

--- roaringbitmap/src/main/java/org/roaringbitmap/RoaringBitmap.java
+++ roaringbitmap/src/main/java/org/roaringbitmap/RoaringBitmap.java
@@ -436,8 +436,9 @@ public class RoaringBitmap implements Cloneable, Serializable, Iterable<Integer>
     }
     return answer;
   }
+
   /**
-   * Set all the specified values  to true. This can be expected to be slightly
+   * Set all the specified values to true. This can be expected to be slightly
    * faster than calling "add" repeatedly. The provided integers values don't
    * have to be in sorted order, but it may be preferable to sort them from a performance point of
    * view.
@@ -445,12 +446,26 @@ public class RoaringBitmap implements Cloneable, Serializable, Iterable<Integer>
    * @param dat set values
    */
   public void add(final int... dat) {
+    this.add(dat, 0, dat.length);
+  }
+
+  /**
+   * Set the specified values to true, within given boundaries. This can be expected to be slightly
+   * faster than calling "add" repeatedly. The provided integers values don't
+   * have to be in sorted order, but it may be preferable to sort them from a performance point of
+   * view.
+   *
+   * @param dat set values
+   * @param offset from which index the values should be set to true
+   * @param length how many values should be set to true
+   */
+  public void add(final int[] dat, final int offset, final int length) {
     Container currentcont = null;
     short currenthb = 0;
     int currentcontainerindex = 0;
     int j = 0;
-    if(j < dat.length) {
-      int val = dat[j];
+    if(j < length) {
+      int val = dat[j + offset];
       currenthb = Util.highbits(val);
       currentcontainerindex = highLowContainer.getIndex(currenthb);
       if (currentcontainerindex >= 0) {
@@ -468,8 +483,8 @@ public class RoaringBitmap implements Cloneable, Serializable, Iterable<Integer>
       }
       j++;
     }
-    for( ; j < dat.length; ++j) {
-      int val = dat[j];
+    for( ; j < length; ++j) {
+      int val = dat[j + offset];
       short newhb = Util.highbits(val);
       if(currenthb == newhb) {// easy case
         // this could be quite frequent
