eee7baa67e07f293bdcf6ddde23dc3a12c7ffce6
Przemek Piotrowski <piotrowski.przemek@gmail.com>
2018-11-08 18:01:55 +0100

    Bitmap statistics & naive writer heuristics recommendation (#280)
    * Fix Random Bitmap generator
    
    * Naive recommendations on builder settings
    
    * Fix failing test
    
    * Post review fixes
    
    * Just in case use longs for statistics
    
    * Coverage improvements

---
+++ roaringbitmap/src/main/java/org/roaringbitmap/insights/NaiveWriterRecommender.java
@@ -0,0 +1,86 @@
+package org.roaringbitmap.insights;
+
+/**
+ * The purpose of this class it to help user decide
+ * which {@link org.roaringbitmap.RoaringBitmapWriter} heuristic to use.
+ */
+public class NaiveWriterRecommender {
+  /**
+   * Based on the statistics it applies expert rules
+   * to help tuning the {@link org.roaringbitmap.RoaringBitmapWriter}
+   */
+  public static String recommend(BitmapStatistics s) {
+    if (s.containerCount() == 0) {
+      return "Empty statistics, cannot recommend.";
+    }
+    StringBuilder sb = new StringBuilder();
+    containerCountRecommendations(s, sb);
+    double acFraction = s.containerFraction(s.getArrayContainersStats().getContainersCount());
+    if (acFraction > ArrayContainersDomination) {
+      if (s.getArrayContainersStats().averageCardinality() < WorthUsingArraysCardinalityThreshold) {
+        arrayContainerRecommendations(s, sb);
+      } else {
+        denseArrayWarning(sb);
+        constantMemoryRecommendation(s, sb);
+      }
+    } else if (s.containerFraction(s.getRunContainerCount()) > RunContainersDomination) {
+      runContainerRecommendations(sb);
+    } else {
+      constantMemoryRecommendation(s, sb);
+    }
+    return sb.toString();
+  }
+
+  private static void denseArrayWarning(StringBuilder sb) {
+    sb
+      .append("Most of your containers are array containers, ")
+      .append("but with quite significant cardinality.\n")
+      .append("It should be better to start with .constantMemory() ")
+      .append("that can scale down to ArrayContainer anyway.");
+  }
+
+  private static void runContainerRecommendations(StringBuilder sb) {
+    sb
+      .append(".optimiseForRuns(), because over ")
+      .append(RunContainersDomination)
+      .append(" containers are of type RunContainer.\n")
+      .append("Make sure to try .constantMemory()")
+      .append("as inserting to RunContainers might not be that efficient.");
+  }
+
+  private static void constantMemoryRecommendation(BitmapStatistics s, StringBuilder sb) {
+    long buffersSizeBytes = s.getBitmapsCount() * Long.BYTES * 1024L;
+    long bufferSizeMiB = buffersSizeBytes / (1024 * 1024);
+    sb
+      .append(".constantMemory() is sensible default for most use cases.\n")
+      .append("Be prepared to allocate on heap ")
+      .append(bufferSizeMiB)
+      .append(" [MiB] just for buffers if you have them open at the same time.");
+  }
+
+  private static void arrayContainerRecommendations(BitmapStatistics s, StringBuilder sb) {
+    double acFraction = s.containerFraction(s.getArrayContainersStats().getContainersCount());
+    sb.append(".optimiseForArrays(), because fraction of ArrayContainers ")
+      .append(acFraction)
+      .append(" is over arbitrary threshold ")
+      .append(ArrayContainersDomination)
+      .append("\n")
+      .append(".expectedContainerSize(")
+      .append(s.getArrayContainersStats().averageCardinality())
+      .append(") to preallocate array containers for average number of elements.\n");
+  }
+
+  private static void containerCountRecommendations(BitmapStatistics basedOn, StringBuilder sb) {
+    long averageContainersCount = basedOn.containerCount() / basedOn.getBitmapsCount();
+    sb.append(".initialCapacity(")
+      .append(averageContainersCount)
+      .append("), because on average each bitmap has ")
+      .append(averageContainersCount)
+      .append(" containers.\n");
+  }
+
+  private static double ArrayContainersDomination = 0.75;
+  private static int WorthUsingArraysCardinalityThreshold = 4096 / 2;
+  private static double RunContainersDomination = 0.8;
+
+}
---
+++ roaringbitmap/src/test/java/org/roaringbitmap/insights/BitmapAnalyserTest.java
@@ -0,0 +1,65 @@
+package org.roaringbitmap.insights;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.roaringbitmap.RandomisedTestData;
+import org.roaringbitmap.RoaringBitmap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class BitmapAnalyserTest {
+
+  @Test
+  public void analyseSingleBitmap() {
+    RoaringBitmap rb = new RoaringBitmap();
+    rb.add(1, 3, 6, 26, 110, 1024);
+    rb.add(70000L, 80000L);
+    for (int i = (5 << 16); i < (6 << 16); i++) {
+      rb.add(i);
+    }
+    BitmapStatistics result = BitmapAnalyser.analyse(rb);
+    BitmapStatistics expected = new BitmapStatistics(new BitmapStatistics.ArrayContainersStats(1, 6), 1, 1);
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void analyseRandomBitmap() {
+    double delta = 0.1;
+    double runFraction = 0.1;
+    double bitmapFraction = 0.2;
+    double denseLimit = runFraction + bitmapFraction;
+    double arrayFraction = 1 - denseLimit;
+    RoaringBitmap rb = RandomisedTestData.randomBitmap(1000, runFraction, denseLimit);
+    BitmapStatistics result = BitmapAnalyser.analyse(rb);
+
+    Assert.assertEquals(runFraction, result.containerFraction(result.getRunContainerCount()), delta);
+    Assert.assertEquals(bitmapFraction, result.containerFraction(result.getBitmapContainerCount()), delta);
+    Assert.assertEquals(arrayFraction, result.containerFraction(result.getArrayContainersStats().getContainersCount()), delta);
+  }
+
+  @Test
+  public void analyseRandomBitmaps() {
+    double delta = 0.1;
+    double runFraction = 0.05;
+    double bitmapFraction = 0.5;
+    double denseLimit = runFraction + bitmapFraction;
+    double arrayFraction = 1 - denseLimit;
+    List<RoaringBitmap> bitmaps = new ArrayList<>();
+    int totalBitmaps = 60;
+    for (int i = 0; i < totalBitmaps; i++) {
+      RoaringBitmap rb = RandomisedTestData.randomBitmap(80, runFraction, denseLimit);
+      bitmaps.add(rb);
+    }
+
+    BitmapStatistics result = BitmapAnalyser.analyse(bitmaps);
+
+    Assert.assertEquals(runFraction, result.containerFraction(result.getRunContainerCount()), delta);
+    Assert.assertEquals(bitmapFraction, result.containerFraction(result.getBitmapContainerCount()), delta);
+    Assert.assertEquals(arrayFraction, result.containerFraction(result.getArrayContainersStats().getContainersCount()), delta);
+    Assert.assertEquals(totalBitmaps, result.getBitmapsCount());
+  }
+
+}
