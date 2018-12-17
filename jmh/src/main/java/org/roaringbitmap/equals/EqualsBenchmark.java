8f062d05e93599f0fafb8cfb920411cd0e5b2f87
Richard Startin <richard@openkappa.co.uk>
2018-12-17 20:15:14 +0000

    Mismatch intrinsic (#290)
    * add benchmarks
    
    * multi-release Arrays.equals
    
    * no need to build under jdk8 now release is fixed at 8, and can't build jdk8 with multirelease versions at 11
    
    * exclude META-INF from JaCoCo for now
    
    * move shims into separate jar to ensure tests run on both versions, shade shims into released artifact
    
    * reorder modules
    
    * avoid releasing shims
    
    * bump shims version after merge
    
    * set manifest entries properly

--- jmh/src/main/java/org/roaringbitmap/equals/EqualsBenchmark.java
+++ jmh/src/main/java/org/roaringbitmap/equals/EqualsBenchmark.java
@@ -1,63 +1,103 @@
 // https://github.com/RoaringBitmap/RoaringBitmap/issues/161
 package org.roaringbitmap.equals;
 
-import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.*;
+import org.roaringbitmap.*;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
-import org.roaringbitmap.ArrayContainer;
-import org.roaringbitmap.Container;
-import org.roaringbitmap.RunContainer;
+import java.util.Arrays;
 
+import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static org.openjdk.jmh.annotations.Mode.AverageTime;
 
-@State(Scope.Benchmark)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@BenchmarkMode(AverageTime)
+@OutputTimeUnit(MICROSECONDS)
 public class EqualsBenchmark {
 
 
-  @BenchmarkMode(Mode.SampleTime)
   @Benchmark
   public boolean runVsArrayEquals_FewRuns(EqualsFewRunsBenchmarkState benchmarkState) {
     return benchmarkState.runContainer.equals(benchmarkState.arrayContainer);
   }
 
-  @BenchmarkMode(Mode.SampleTime)
   @Benchmark
   public boolean runVsArrayEquals_ManyRuns(EqualsManyRunsBenchmarkState benchmarkState) {
     return benchmarkState.runContainer.equals(benchmarkState.arrayContainer);
   }
 
+  @Benchmark
+  public boolean arrayVsArrayEquals(EqualArrayContainersBenchmarkState state) {
+    return state.left.equals(state.right);
+  }
+
+  @Benchmark
+  public boolean equalBitmaps(EqualBitmapsState state) {
+    return state.left.equals(state.right);
+  }
 
   @State(Scope.Benchmark)
   public static class EqualsFewRunsBenchmarkState {
 
-    final RunContainer runContainer = new RunContainer();
-    final ArrayContainer arrayContainer = new ArrayContainer();
+    Container runContainer = new RunContainer();
+    Container arrayContainer = new ArrayContainer();
 
     public EqualsFewRunsBenchmarkState() {
-      addRange(arrayContainer, 20, 400);
-      addRange(runContainer, 20, 400);
+      arrayContainer = addRange(arrayContainer, 20, 400);
+      runContainer = addRange(runContainer, 20, 400);
 
-      addRange(arrayContainer, 501, 1500);
-      addRange(runContainer, 501, 1500);
+      arrayContainer = addRange(arrayContainer, 501, 1500);
+      runContainer = addRange(runContainer, 501, 1500);
 
 
-      addRange(arrayContainer, 3000, 4500);
-      addRange(runContainer, 3000, 4500);
+      arrayContainer = addRange(arrayContainer, 3000, 4500);
+      runContainer = addRange(runContainer, 3000, 4500);
     }
 
   }
 
+  @State(Scope.Benchmark)
+  public static class EqualBitmapsState {
+
+    RoaringBitmap left;
+    RoaringBitmap right;
+
+    @Setup(Level.Trial)
+    public void init() {
+      left = RandomData.randomBitmap(50, 0.5, 0.5);
+      right = left.clone();
+    }
+  }
+
+
+  @State(Scope.Benchmark)
+  public static class EqualArrayContainersBenchmarkState {
+    @Param({"20", "200", "1000", "2000"})
+    int size;
+    @Param({"0.1", "0.5", "0.9", "1"})
+    float firstMismatch;
+
+    Container left;
+    Container right;
+
+    @Setup(Level.Trial)
+    public void init() {
+      short[] l = array(size);
+      short[] r = Arrays.copyOf(l, size);
+      int mismatch = (int) (firstMismatch * size);
+      if (mismatch < size) {
+        for (int i = 0; i < size; ++i) {
+          r[i] += 4096;
+        }
+      }
+      left = new ArrayContainer(size, l);
+      right = new ArrayContainer(size, r);
+    }
+  }
 
   @State(Scope.Benchmark)
   public static class EqualsManyRunsBenchmarkState {
 
-    final RunContainer runContainer = new RunContainer();
-    final ArrayContainer arrayContainer = new ArrayContainer();
+    Container runContainer = new RunContainer();
+    Container arrayContainer = new ArrayContainer();
 
     public EqualsManyRunsBenchmarkState() {
       int cardinality = 0;
@@ -66,8 +106,8 @@ public class EqualsBenchmark {
       int runStart = 0;
       while (cardinality < 4096 - runLength) {
         cardinality += runLength;
-        addRange(runContainer, runStart, runStart + runLength);
-        addRange(arrayContainer, runStart, runStart + runLength);
+        runContainer = addRange(runContainer, runStart, runStart + runLength);
+        arrayContainer = addRange(arrayContainer, runStart, runStart + runLength);
         runStart += runLength + shift;
       }
     }
@@ -75,8 +115,19 @@ public class EqualsBenchmark {
   }
 
 
-  static void addRange(Container rc, int min, int sup) {
-    rc.iadd(min, sup);
+  private static short[] array(int size) {
+    if (size >= 4096) {
+      throw new IllegalStateException();
+    }
+    short[] array = new short[size];
+    for (int i = 0; i < size; ++i) {
+      array[i] = (short) i;
+    }
+    return array;
+  }
+
+  static Container addRange(Container c, int min, int sup) {
+    return c.iadd(min, sup);
   }
 
 }
