5e04b7ff497588197f9ca8e86be7f63b749242d4
Richard Startin <richard@openkappa.co.uk>
2018-12-21 14:21:01 +0000

    Shims javadoc (#298)
    * add javdoc for ArraysShim methods
    
    * skip javadoc plugin in shims since the classes are not public

--- shims/src/main/java11/org/roaringbitmap/ArraysShim.java
+++ shims/src/main/java11/org/roaringbitmap/ArraysShim.java
@@ -4,6 +4,16 @@ import java.util.Arrays;
 
 class ArraysShim {
 
+  /**
+   * Checks if the two arrays are equal within the given range.
+   * @param x the first array
+   * @param xmin the inclusive minimum of the range of the first array
+   * @param xmax the exclusive maximum of the range of the first array
+   * @param y the second array
+   * @param ymin the inclusive minimum of the range of the second array
+   * @param ymax the exclusive maximum of the range of the second array
+   * @return true if the arrays are equal in the specified ranges
+   */
   public static boolean equals(short[] x, int xmin, int xmax, short[] y, int ymin, int ymax) {
     return Arrays.equals(x, xmin, xmax, y, ymin, ymax);
   }
