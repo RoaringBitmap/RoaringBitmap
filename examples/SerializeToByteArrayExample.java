017cc96e21da274c04996c146d9867729671a3c2
Benoit Lacelle <benoit.lacelle@gmail.com>
2019-03-18 21:14:36 +0100

    Fix checkstyle

--- examples/SerializeToByteArrayExample.java
+++ examples/SerializeToByteArrayExample.java
@@ -1,10 +1,9 @@
 /**
-* This example shows how to serialize a Roaring bitmap to a byte array 
-*/
-/*************** 
-* for performance considerations, see
-* https://github.com/RoaringBitmap/RoaringBitmap/issues/319
-**************/
+ * This example shows how to serialize a Roaring bitmap to a byte array
+ */
+/***************
+ * for performance considerations, see https://github.com/RoaringBitmap/RoaringBitmap/issues/319
+ **************/
 import org.roaringbitmap.RoaringBitmap;
 import java.io.*;
 import java.nio.*;
@@ -13,54 +12,51 @@ public class SerializeToByteArrayExample {
 
 
 
+  public static void main(String[] args) {
+    RoaringBitmap mrb = RoaringBitmap.bitmapOf(1, 2, 3, 1000);
+    System.out.println("starting with  bitmap " + mrb);
+    mrb.runOptimize(); // to improve compression
+    byte[] array = new byte[mrb.serializedSizeInBytes()];
+    try {
+      mrb.serialize(new java.io.DataOutputStream(new java.io.OutputStream() {
+        int c = 0;
 
+        @Override
+        public void close() {}
 
-    public static void main(String[] args)  {
-        RoaringBitmap mrb = RoaringBitmap.bitmapOf(1,2,3,1000);
-        System.out.println("starting with  bitmap "+ mrb);
-        mrb.runOptimize(); //to improve compression
-        byte[] array = new byte[mrb.serializedSizeInBytes()];
-        try {
-            mrb.serialize(new java.io.DataOutputStream(new java.io.OutputStream() {
-                int c = 0;
+        @Override
+        public void flush() {}
 
-                @Override
-                public void close() {
-                }
-
-                @Override
-                public void flush() {
-                }
-
-                @Override
-                public void write(int b) {
-                    array[c++] = (byte)b;
-                }
-
-                @Override
-                public void write(byte[] b) {
-                    write(b,0,b.length);
-                }
-
-                @Override
-                public void write(byte[] b, int off, int l) {
-                    System.arraycopy(b, off, array, c, l);
-                    c += l;
-                }
-            }));
-        } catch (IOException ioe) {
-            // should never happen because we write to a byte array
-            throw new RuntimeException("unexpected error while serializing to a byte array");
+        @Override
+        public void write(int b) {
+          array[c++] = (byte) b;
         }
-        RoaringBitmap ret = new RoaringBitmap();
-        try {
-            ret.deserialize(ByteBuffer.wrap(array));
-        } catch (IOException ioe) {
-            // should never happen because we read from a byte array
-            throw new RuntimeException("unexpected error while deserializing from a byte array");
-        }
-        if(!ret.equals(mrb)) throw new RuntimeException("bug");
-        System.out.println("decoded from byte array : "+ret);
 
+        @Override
+        public void write(byte[] b) {
+          write(b, 0, b.length);
+        }
+
+        @Override
+        public void write(byte[] b, int off, int l) {
+          System.arraycopy(b, off, array, c, l);
+          c += l;
+        }
+      }));
+    } catch (IOException ioe) {
+      // should never happen because we write to a byte array
+      throw new RuntimeException("unexpected error while serializing to a byte array");
     }
+    RoaringBitmap ret = new RoaringBitmap();
+    try {
+      ret.deserialize(ByteBuffer.wrap(array));
+    } catch (IOException ioe) {
+      // should never happen because we read from a byte array
+      throw new RuntimeException("unexpected error while deserializing from a byte array");
+    }
+    if (!ret.equals(mrb))
+      throw new RuntimeException("bug");
+    System.out.println("decoded from byte array : " + ret);
+
+  }
 }
