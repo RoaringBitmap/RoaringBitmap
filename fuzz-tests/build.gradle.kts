29979ef6ca93cbb765234c967724a8f932a3c7d4
Marshall Pierce <marshall@mpierce.org>
2019-05-26 17:32:53 -0600

    Disable fuzz tests unless roaringbitmap.fuzz-tests prop is set

--- fuzz-tests/build.gradle.kts
+++ fuzz-tests/build.gradle.kts
@@ -6,3 +6,10 @@ dependencies {
     testImplementation("com.google.guava:guava:${deps["guava"]}")
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.9.9")
 }
+
+tasks.test {
+    // set the property on the CLI with -P or add to gradle.properties to enable tests
+    if (!project.hasProperty("roaringbitmap.fuzz-tests")) {
+        exclude("**")
+    }
+}
