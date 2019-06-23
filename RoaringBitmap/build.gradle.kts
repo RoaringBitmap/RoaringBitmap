446e6a38581d01f58e436dcb7b78a195156ce6e3
Richard Startin <richard@openkappa.co.uk>
2019-06-23 14:56:41 +0100

    Test/build config (#348)
    * checkstyle runs first, checkstyle fails build, jmh tests run after roaring tests, parallelise roaring and jmh tests
    
    * softer task ordering constraints

--- RoaringBitmap/build.gradle.kts
+++ RoaringBitmap/build.gradle.kts
@@ -8,3 +8,11 @@ dependencies {
     testImplementation("org.apache.commons:commons-lang3:${deps["commons-lang"]}")
     testImplementation("com.esotericsoftware:kryo:5.0.0-RC1")
 }
+
+
+tasks.test {
+    mustRunAfter(tasks.checkstyleMain)
+    useJUnit()
+    failFast = true
+    maxParallelForks = 8
+}
