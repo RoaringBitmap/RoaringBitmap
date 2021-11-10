eb960c4dc58aba8d0e716ac1de971750c1fb62bb
richardstartin <richard@startree.ai>
2021-11-10 11:06:45 +0000

    avoid checking if bitmap has run compression more than once, avoid some duplication, replace some PascalCase with camelCase

--- RoaringBitmap/src/main/java/org/roaringbitmap/buffer/ImmutableRoaringArray.java
+++ RoaringBitmap/src/main/java/org/roaringbitmap/buffer/ImmutableRoaringArray.java
@@ -43,14 +43,13 @@ public final class ImmutableRoaringArray implements PointableRoaringArray {
   protected ImmutableRoaringArray(ByteBuffer bbf) {
     buffer = bbf.slice();
     buffer.order(ByteOrder.LITTLE_ENDIAN);
-    final int cookie = buffer.getInt(0);
-    if ((cookie & 0xFFFF) != SERIAL_COOKIE && cookie != SERIAL_COOKIE_NO_RUNCONTAINER) {
+    int cookie = buffer.getInt(0);
+    boolean hasRunContainers = (cookie & 0xFFFF) == SERIAL_COOKIE;
+    if (!hasRunContainers && cookie != SERIAL_COOKIE_NO_RUNCONTAINER) {
       throw new InvalidRoaringFormat("I failed to find one of the right cookies. " + cookie);
     }
-    boolean hasRunContainers = (cookie & 0xFFFF) == SERIAL_COOKIE;
     this.size = hasRunContainers ? (cookie >>> 16) + 1 : buffer.getInt(4);
-    int theLimit = size > 0 ? computeSerializedSizeInBytes() : headerSize(hasRunContainers);
-    buffer.limit(theLimit);
+    buffer.limit(computeSerializedSizeInBytes(hasRunContainers));
   }
 
   @Override
@@ -130,31 +129,30 @@ public final class ImmutableRoaringArray implements PointableRoaringArray {
   }
 
 
-  private int computeSerializedSizeInBytes() {
+  private int computeSerializedSizeInBytes(boolean hasRunContainers) {
     if (this.size == 0) {
-      return headerSize(hasRunCompression());
+      return headerSize(hasRunContainers);
     }
-    int CardinalityOfLastContainer = getCardinality(this.size - 1);
-    int PositionOfLastContainer = getOffsetContainer(this.size - 1);
-    int SizeOfLastContainer;
-    boolean hasrun = hasRunCompression();
-    if (isRunContainer(this.size - 1, hasrun)) {
-      int nbrruns = (buffer.getChar(PositionOfLastContainer));
-      SizeOfLastContainer = BufferUtil.getSizeInBytesFromCardinalityEtc(0, nbrruns, true);
+    int positionOfLastContainer = getOffsetContainer(this.size - 1, hasRunContainers);
+    int sizeOfLastContainer;
+    if (isRunContainer(this.size - 1, hasRunContainers)) {
+      int nbrruns = buffer.getChar(positionOfLastContainer);
+      sizeOfLastContainer = BufferUtil.getSizeInBytesFromCardinalityEtc(0, nbrruns, true);
     } else {
-      SizeOfLastContainer =
-          BufferUtil.getSizeInBytesFromCardinalityEtc(CardinalityOfLastContainer, 0, false);
+      int cardinalityOfLastContainer = getCardinality(this.size - 1);
+      sizeOfLastContainer =
+          BufferUtil.getSizeInBytesFromCardinalityEtc(cardinalityOfLastContainer, 0, false);
     }
-    return SizeOfLastContainer + PositionOfLastContainer;
+    return sizeOfLastContainer + positionOfLastContainer;
   }
 
   @Override
   public int getCardinality(int k) {
-    if ((k < 0) || (k >= this.size)) {
+    if (k < 0 || k >= this.size) {
       throw new IllegalArgumentException(
           "out of range container index: " + k + " (report as a bug)");
     }
-    return (buffer.getChar(this.getStartOfKeys() + 4 * k + 2)) + 1;
+    return buffer.getChar(this.getStartOfKeys() + 4 * k + 2) + 1;
   }
 
 
@@ -166,12 +164,12 @@ public final class ImmutableRoaringArray implements PointableRoaringArray {
 
   @Override
   public MappeableContainer getContainerAtIndex(int i) {
+    boolean hasrun = hasRunCompression();
     ByteBuffer tmp = buffer.duplicate();// sad but ByteBuffer is not thread-safe so it is either a
                                         // duplicate or a lock
     // note that tmp will indeed be garbage-collected some time after the end of this function
     tmp.order(buffer.order());
-    tmp.position(getOffsetContainer(i));
-    boolean hasrun = hasRunCompression();
+    tmp.position(getOffsetContainer(i, hasrun));
     if (isRunContainer(i, hasrun)) {
       // first, we have a char giving the number of runs
       int nbrruns = (tmp.getChar());
@@ -195,8 +193,8 @@ public final class ImmutableRoaringArray implements PointableRoaringArray {
 
   @Override
   public boolean containsForContainerAtIndex(int i, char x) {
-    int containerpos = getOffsetContainer(i);
     boolean hasrun = hasRunCompression();
+    int containerpos = getOffsetContainer(i, hasrun);
     if (isRunContainer(i, hasrun)) {
       // first, we have a char giving the number of runs
       int nbrruns = (buffer.getChar(containerpos));
@@ -266,7 +264,7 @@ public final class ImmutableRoaringArray implements PointableRoaringArray {
       public int getSizeInBytes() {
         // might be a tad expensive
         if (ImmutableRoaringArray.this.isRunContainer(k, hasrun)) {
-          int pos = getOffsetContainer(k);
+          int pos = getOffsetContainer(k, true);
           int nbrruns = (buffer.getChar(pos));
           return BufferUtil.getSizeInBytesFromCardinalityEtc(0, nbrruns, true);
         } else {
@@ -322,15 +320,15 @@ public final class ImmutableRoaringArray implements PointableRoaringArray {
     return buffer.getChar(4 * i + getStartOfKeys());
   }
 
-  private int getOffsetContainer(int k) {
-    if ((k < 0) || (k >= this.size)) {
+  private int getOffsetContainer(int k, boolean hasRunCompression) {
+    if (k < 0 || k >= this.size) {
       throw new IllegalArgumentException(
           "out of range container index: " + k + " (report as a bug)");
     }
-    if (hasRunCompression()) { // account for size of runcontainer bitmap
+    if (hasRunCompression) { // account for size of runcontainer bitmap
       if (this.size < MutableRoaringArray.NO_OFFSET_THRESHOLD) {
         // we do it the hard way
-        return getOffsetContainerSlow(k);
+        return getOffsetContainerSlow(k, true);
       }
       return buffer.getInt(4 + 4 * this.size + ((this.size + 7) / 8) + 4 * k);
     } else {
@@ -339,19 +337,18 @@ public final class ImmutableRoaringArray implements PointableRoaringArray {
   }
 
 
-  private int getOffsetContainerSlow(int k) {
-    boolean hasrun = hasRunCompression();
-    int pos = this.headerSize(hasrun);
+  private int getOffsetContainerSlow(int k, boolean hasRunCompression) {
+    int pos = this.headerSize(hasRunCompression);
     for (int z = 0; z < k; ++z) {
-      if (isRunContainer(z, hasrun)) {
-        int nbrruns = (buffer.getChar(pos));
-        int SizeOfLastContainer = BufferUtil.getSizeInBytesFromCardinalityEtc(0, nbrruns, true);
-        pos += SizeOfLastContainer;
+      if (isRunContainer(z, hasRunCompression)) {
+        int nbrruns = buffer.getChar(pos);
+        int sizeOfLastContainer = BufferUtil.getSizeInBytesFromCardinalityEtc(0, nbrruns, true);
+        pos += sizeOfLastContainer;
       } else {
-        int CardinalityOfLastContainer = this.getCardinality(z);
-        int SizeOfLastContainer =
-            BufferUtil.getSizeInBytesFromCardinalityEtc(CardinalityOfLastContainer, 0, false);
-        pos += SizeOfLastContainer;
+        int cardinalityOfLastContainer = this.getCardinality(z);
+        int sizeOfLastContainer =
+            BufferUtil.getSizeInBytesFromCardinalityEtc(cardinalityOfLastContainer, 0, false);
+        pos += sizeOfLastContainer;
       }
     }
     return pos;
