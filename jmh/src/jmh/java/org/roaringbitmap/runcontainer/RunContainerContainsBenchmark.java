b626130141c055e66827d05a06c594518be57722
xtonik <antonin.janec@gmail.com>
2024-01-13 23:55:24 +0100

    benchmark correction

--- jmh/src/jmh/java/org/roaringbitmap/runcontainer/RunContainerContainsBenchmark.java
+++ jmh/src/jmh/java/org/roaringbitmap/runcontainer/RunContainerContainsBenchmark.java
@@ -7,8 +7,8 @@ import org.roaringbitmap.buffer.MappeableRunContainer;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 6, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
@@ -16,27 +16,26 @@ public class RunContainerContainsBenchmark {
 
   public MappeableRunContainer rc;
   public MappeableBitmapContainer bc;
+
   @Setup
   public void setup() {
     rc = new MappeableRunContainer();
     bc = new MappeableBitmapContainer();
     Random r = new Random(0);
     int begin, end = 0;
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 50; i++) {
       begin = end + r.nextInt(10);
       end = begin + r.nextInt(100) + 1;
-      rc.add(begin, end);
-      System.out.println("range added:" + begin + " " + end);
-
-      bc.add((char) (begin + r.nextInt(end - begin - 1)));
-      bc.add((char) (begin + r.nextInt(end - begin - 1)));
-      bc.add((char) (begin + r.nextInt(end - begin - 1)));
-      bc.add((char) (begin + r.nextInt(end - begin - 1)));
+      rc = (MappeableRunContainer) rc.add(begin, end);
+      //System.out.println("range added:" + begin + " " + end);
+      for (int j = 0; j < 8; j++) {
+        bc = (MappeableBitmapContainer) bc.add((char) (begin + r.nextInt(end - begin - 1)));
+      }
     }
-    System.out.println("BC: " + bc.toString());
-    System.out.println("RC: " + rc.toString());
-    System.out.println(rc.contains(bc) + " " + rc.contains2(bc));
-    System.err.println("================================");
+    //System.out.println("BC: " + bc.toString());
+    //System.out.println("RC: " + rc.toString() + " " + rc.numberOfRuns());
+    System.out.println("CONTAINS: " + rc.contains(bc) + " " + rc.contains2(bc));
+    //System.err.println("================================");
   }
 
   @Benchmark
