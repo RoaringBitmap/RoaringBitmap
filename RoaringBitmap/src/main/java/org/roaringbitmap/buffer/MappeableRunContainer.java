003fe2cc2362d3ce4b9ad826efc4d7c0ffefc64b
xtonik <antonin.janec@gmail.com>
2024-01-27 17:57:46 +0100

    removed comparison for optimized contains() of (Mappeable)RunContainer

--- RoaringBitmap/src/main/java/org/roaringbitmap/buffer/MappeableRunContainer.java
+++ RoaringBitmap/src/main/java/org/roaringbitmap/buffer/MappeableRunContainer.java
@@ -2776,51 +2776,6 @@ public final class MappeableRunContainer extends MappeableContainer implements C
     return true;
   }
 
-  /**
-   * It exists for performance comparison with original implementation only. It should be deleted
-   * before merge to master branch.
-   *
-   * @param bitmapContainer container
-   * @return true if given container is subset of this container
-   */
-  public boolean contains2(MappeableBitmapContainer bitmapContainer) {
-    final int cardinality = getCardinality();
-    if (bitmapContainer.getCardinality() != -1 && bitmapContainer.getCardinality() > cardinality) {
-      return false;
-    }
-    final int runCount = numberOfRuns();
-    char ib = 0, ir = 0;
-    while(ib < MappeableBitmapContainer.MAX_CAPACITY / 64 && ir < runCount) {
-      long w = bitmapContainer.bitmap.get(ib);
-      while (w != 0 && ir < runCount) {
-        int start = (getValue(ir));
-        int stop = start+ (getLength(ir));
-        long t = w & -w;
-        long r = ib * 64 + Long.numberOfTrailingZeros(w);
-        if (r < start) {
-          return false;
-        } else if(r > stop) {
-          ++ir;
-        } else {
-          w ^= t;
-        }
-      }
-      if(w == 0) {
-        ++ib;
-      } else {
-        return false;
-      }
-    }
-    if(ib < MappeableBitmapContainer.MAX_CAPACITY / 64) {
-      for(; ib < MappeableBitmapContainer.MAX_CAPACITY / 64 ; ib++) {
-        if(bitmapContainer.bitmap.get(ib) != 0) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
   @Override
   public boolean intersects(int minimum, int supremum) {
     if((minimum < 0) || (supremum < minimum) || (supremum > (1<<16))) {
