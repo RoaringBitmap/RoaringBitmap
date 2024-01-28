a81f12ca00c60175888dee5070dce88d25b3ec1d
xtonik <antonin.janec@gmail.com>
2024-01-28 18:05:55 +0100

    Avoid useless computation in oNeilCompare() (#702)
    * avoid useless operations in oNeilCompare()
    
    * avoid instantiation unused empty bitmaps

--- bsi/src/main/java/org/roaringbitmap/bsi/buffer/BitSliceIndexBase.java
+++ bsi/src/main/java/org/roaringbitmap/bsi/buffer/BitSliceIndexBase.java
@@ -6,6 +6,7 @@ import org.roaringbitmap.IntConsumer;
 import org.roaringbitmap.IntIterator;
 import org.roaringbitmap.RoaringBitmap;
 import org.roaringbitmap.bsi.BitmapSliceIndex;
+import org.roaringbitmap.bsi.BitmapSliceIndex.Operation;
 import org.roaringbitmap.bsi.Pair;
 import org.roaringbitmap.buffer.BufferFastAggregation;
 import org.roaringbitmap.buffer.ImmutableRoaringBitmap;
@@ -191,23 +192,31 @@ public class BitSliceIndexBase {
                         ImmutableRoaringBitmap foundSet) {
     ImmutableRoaringBitmap fixedFoundSet = foundSet == null ? this.ebM : foundSet;
 
-    MutableRoaringBitmap GT = new MutableRoaringBitmap();
-    MutableRoaringBitmap LT = new MutableRoaringBitmap();
+    MutableRoaringBitmap GT = operation == Operation.GT || operation == Operation.GE
+        ? new MutableRoaringBitmap() : null;
+    MutableRoaringBitmap LT = operation == Operation.LT || operation == Operation.LE
+        ? new MutableRoaringBitmap() : null;
     ImmutableRoaringBitmap EQ = this.ebM;
 
 
     for (int i = this.bitCount() - 1; i >= 0; i--) {
       int bit = (predicate >> i) & 1;
       if (bit == 1) {
-        LT = ImmutableRoaringBitmap.or(LT, ImmutableRoaringBitmap.andNot(EQ, this.bA[i]));
+        if (operation == Operation.LT || operation == Operation.LE) {
+          LT = ImmutableRoaringBitmap.or(LT, ImmutableRoaringBitmap.andNot(EQ, this.bA[i]));
+        }
         EQ = ImmutableRoaringBitmap.and(EQ, this.bA[i]);
       } else {
-        GT = ImmutableRoaringBitmap.or(GT, ImmutableRoaringBitmap.and(EQ, this.bA[i]));
+        if (operation == Operation.GT || operation == Operation.GE) {
+          GT = ImmutableRoaringBitmap.or(GT, ImmutableRoaringBitmap.and(EQ, this.bA[i]));
+        }
         EQ = ImmutableRoaringBitmap.andNot(EQ, this.bA[i]);
       }
 
     }
-    EQ = ImmutableRoaringBitmap.and(fixedFoundSet, EQ);
+    if (operation != Operation.LT && operation != Operation.GT) {
+      EQ = ImmutableRoaringBitmap.and(fixedFoundSet, EQ);
+    }
     switch (operation) {
       case EQ:
         return EQ;
